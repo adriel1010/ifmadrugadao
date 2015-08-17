91b60cb5fd14d7e921e9780146b51c9d66f64b84
adriel <adriel@adriel.ifprpvai.lan>
2015-08-17 08:58:05 -0300

    criando painel ADM

--- controlador.php
+++ controlador.php
@@ -1,5 +1,19 @@
 <?php require './visoes/menu.php';?>
 
-Controlador
-
+<form id="testando" >
+    
+    <h4 id="paineladm"> Painel Administrativo</h4>
+     <p>
+                <input name="usuario" type="text" required="true" placeholder="Usuario"/>
+            </p>
+            <p>
+                <input name="senha" type="text" required="true" placeholder="Senha"/>
+            </p>
+           
+            <p >
+                <input type="submit" value="Logar"/>
+            </p>
+            
+        
+</form>
 <?php require './visoes/rodape.php';?>
