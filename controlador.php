513875714b6b9aea2056d2420d4bd39cb8959d3a
Gabriel <Gabriel@192.168.1.7>
2015-08-21 18:12:39 -0300

    estilizando controlador

--- controlador.php
+++ controlador.php
@@ -1,20 +1,29 @@
 <?php
     require './visoes/topo.php';
     require './visoes/menu.php';
+    require './visoes/arte.php';
 ?>
+<div class="row">
+    <div class="col-xs-3 col-md-3 col-sm-3">
+    </div>
+    <div class="col-xs-6 col-md-6 col-sm-6">
+        <form id="login"  action="banco/conexao.php" method="post">
+            <h4><center>Painel Administrativo</center></h4>
+            <p>
+                <input name="usuario" type="text" required="true" placeholder="Usuário"/>
+            </p>
+            <p>
+                <input name="senha" type="password" required="true" placeholder="Senha"/>
+            </p>
+            <p>
+                <input type="submit" value="Logar"/>
+                <input type="button" value="Limpar"/>
+            </p>
+        </form>
+    </div>
+    <div class="col-xs-3 col-md-3 col-sm-3">
+    </div>
+</div>
 
-<form id="testando"  action="banco/conexao.php" method="post"  >
-    
-    <h4 id="paineladm"> Painel Administrativo</h4>
-    <p>
-    <input name="usuario" type="text" required="true" placeholder="Usuario"/>
-    </p>
-    <p>
-    <input name="senha" type="password" required="true" placeholder="Senha"/>
-    </p>
-    <p>
-    <input type="submit" value="Logar"/>
-    </p>
-   
-</form>
+
 <?php require './visoes/rodape.php';?>
